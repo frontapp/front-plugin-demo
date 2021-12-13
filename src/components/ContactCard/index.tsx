7d8387481ac8bd66d45173ed5c26cf451884a5e3
vnagirniak <88381755+vnagirniak@users.noreply.github.com>
2021-12-13 09:01:02 +0200

    INT-158 Changed some code regarding update out lib (#18)
    Co-authored-by: Vadim Nagirniak <vadim.nagirniak@irco.com>

--- src/components/ContactCard/index.tsx
+++ src/components/ContactCard/index.tsx
@@ -1,5 +1,5 @@
 import React  from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import {
 	ItemCard,
 } from '@frontapp/plugin-components';
@@ -13,20 +13,24 @@ interface ContactCardProps {
 }
 
 const ContactCard: React.FC<ContactCardProps> = ({ contact , id}) => {
+	const history = useHistory();
+
+	const goToItem = (id: string) => {
+		history.push(`/items/${id}`);
+	};
+
 	return (
-		<Link className='contact-cart-link-wrapper' to={`/items/${id}`}>
-			<ItemCard title={contact['Full Name']} width={'100%'} className="contact-card">
-				<div className="contact-card-body-line">
-					<span className="item-card-body-line-text">{contact['Email']}</span>
-				</div>
-				<div className="contact-card-body-line">
-					<span className="item-card-body-line-text">{contact['Phone']}</span>
-				</div>
-				<div className="contact-card-body-line">
-					<span className="item-card-body-line-text">{contact['Title']}</span>
-				</div>
-			</ItemCard>
-		</Link>
+		<ItemCard title={contact['Full Name']} width={'100%'} className="contact-card" onClick={() => goToItem(id)}>
+			<div className="contact-card-body-line">
+				<span className="item-card-body-line-text">{contact['Email']}</span>
+			</div>
+			<div className="contact-card-body-line">
+				<span className="item-card-body-line-text">{contact['Phone']}</span>
+			</div>
+			<div className="contact-card-body-line">
+				<span className="item-card-body-line-text">{contact['Title']}</span>
+			</div>
+		</ItemCard>
 	);
 };
 
